dafeae39d1002fd6f993b515dd7c6b1005008599
Caleb Heinz <caleb.heinz@7factor.io>
2024-04-07 15:53:28 -0400

    Added profile endpoint to backend.

---
+++ backend/src/data/profile.ts
@@ -0,0 +1,10 @@
+import { ProfileDTO } from "../types/api";
+
+export const Profiles: Record<number, ProfileDTO> = {
+  1: {
+    id: 1,
+    firstName: "Caleb",
+    lastName: "Heinz",
+    favoriteColor: "red",
+  },
+};
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,12 +1,20 @@
-import express, {Express, Request, Response} from 'express';
+import express, { Express, Request, Response } from "express";
+import cors from "cors";
+import { Profiles } from "./data/profile";
 
 const app: Express = express();
+app.use(cors());
+
 const port = 3001;
 
-app.get('/', (req: Request, res: Response)=>{
-  res.send('Hello, this is Express + TypeScript');
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello, this is Express + TypeScript");
 });
 
-app.listen(port, ()=> {
+app.get("/user/:id", (req: Request<{ id: number }>, res: Response) => {
+  res.send(Profiles[req.params.id]);
+});
+
+app.listen(port, () => {
   console.log(`[Server]: I am running at https://localhost:${port}`);
 });

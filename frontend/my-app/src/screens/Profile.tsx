78aebaf27beb05bf48198a8ec47101415ab5f0fd
Caleb Heinz <caleb.heinz@7factor.io>
2024-04-28 14:47:47 -0400

    Adding profile page

--- frontend/my-app/src/screens/Profile.tsx
+++ frontend/my-app/src/screens/Profile.tsx
@@ -1,7 +1,38 @@
+import { useEffect, useState } from "react";
+import { getProfileById } from "../data/api";
+import { Container, Row } from "react-bootstrap";
+
 export function ProfileScreen() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [favoriteColor, setFavoriteColor] = useState("");
+
+  useEffect(() => {
+    getProfileById(1).then((profile) => {
+      setFirstName(profile.firstName);
+      setLastName(profile.lastName);
+      setFavoriteColor(profile.favoriteColor);
+      setIsLoading(false);
+    });
+  }, []);
+
   return (
     <>
-      <h1>Profile Screen</h1>
+      {isLoading ? (
+        <div>Loading...</div>
+      ) : (
+        <Container>
+          <Row>
+            <h1>Profile Screen</h1>
+          </Row>
+          <Row>First Name: {firstName}</Row>
+          <Row>Last Name: {lastName}</Row>
+          <Row style={{ color: favoriteColor }}>
+            Favorite Color: {favoriteColor}
+          </Row>
+        </Container>
+      )}
     </>
   );
 }
